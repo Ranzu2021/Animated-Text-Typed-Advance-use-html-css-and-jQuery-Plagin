18549928a0d158884da653396150561cad9cd956
Ranzu2021 <79698129+Ranzu2021@users.noreply.github.com>
2021-02-28 15:41:51 +0600

    Added html ,css and jQuery Plagin file

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var typed = new Typed('.text', {
+        strings: [
+            'Hello!!', 'My Name Is RANZU.', 'I Am A Web   Designer.', 'I Am A Web Developper.', 'I Am A UX / Ui Designer...'
+        ],
+        typeSpeed: 70,
+        startDelay: 500,
+        backDelay: 200,
+        loop: true,
+        smartBackspace: true,
+        cursorChar: '|'
+      });
+});
